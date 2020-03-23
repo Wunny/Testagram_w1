013d5d819dc80d31376822bea16b2a3619af1f5f
HOME <dndnjs511@naver.com>
2020-03-23 19:02:14 +0900

    first commit

---
+++ app/src/main/java/com/example/testagram_w1/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testagram_w1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
